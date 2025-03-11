b618fbd449ca74b400082d8f524092b29679871d
Fabio <navarro.fabio@gmail.com>
2025-03-10 22:47:27 -0300

    inicial

---
+++ DDDCommerceComRepository.Infra/SqlContext.cs
@@ -0,0 +1,25 @@
+﻿using DDDCommerceComRepository.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDCommerceComRepository.Infra
+{
+    public class SqlContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ECommerceDb01");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+    }
+}
